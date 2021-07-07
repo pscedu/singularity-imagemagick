7ec5cc429a1813412c712dd51c3740b0a0b7d3b1
icaoberg <icaoberg@andrew.cmu.edu>
2021-07-07 16:20:17 -0400

    Update modulefile.lua

--- 7.1.0-2/modulefile.lua
+++ 7.1.0-2/modulefile.lua
@@ -12,17 +12,18 @@ whatis("URL: https://www.psc.edu/resources/software")
 whatis("Description: ImageMagick is a free and open-source cross-platform software suite for displaying, creating, converting, modifying, and editing raster images. ")
 
 help([[
-imagemagick 7.1.0-2
------------
-This program computes the cross entropy for groups of sequences that have been assigned to groups on the basis of biochemical, physiological, or other biological property. The sequence assignments are cross-validated, again by the cross entropy measure, to check for problems with the alignment or group assignment.  Sequences that were initially identified as "unclassified" are compared to all of the groups using position specific log-odds scores as described by Henikoff and Henikoff. Positions in the aligned sequences that are important for determining group membership are identified by having a high entropy for the entire alignment and a high entropy for one or more specific groups.
 Description
 -----------
+ImageMagick is a free and open-source cross-platform software suite for displaying, creating, converting, modifying, and editing raster images.
+  
 To load the module, type
+
 > module load imagemagick/7.1.0-2
+
 To unload the module, type
+
 > module unload imagemagick/7.1.0-2
-Repository
-----------
+
 Tools included in this module are
 * imagemagick
 ]])
