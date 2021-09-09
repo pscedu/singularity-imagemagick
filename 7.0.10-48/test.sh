0d90ed108c574e1fc7acfe5c0b8e4e807f700cf6
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2021-09-08 23:23:00 -0400

    Updated test

--- 7.0.10-48/test.sh
+++ 7.0.10-48/test.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+set -x
+
+tree .
+
+wget -nc -O image.jpg https://pixabay.com/get/gda63317ad0c80d23ad8adda3ac5744082abd11c2021f366739f037c9ed2234f56597d7344347416b5f159fc533396ed5_1920.jpg
+singularity exec singularity-imagemagick-7.0.10-48.sif convert image.jpg -resize 50% image.jpg 
